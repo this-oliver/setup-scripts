422883c4a0c1d701a58a1d7df56ec0190763cda9
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-01-27 00:26:26 +0100

    adds input validation

--- install.sh
+++ install.sh
@@ -1,57 +1,116 @@
 # get path to script directory
 curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 script_dir="$curr_dir/scripts"
-setup-basics="$script_dir/setup-basics.sh"
-setup-docker="$script_dir/setup-docker.sh"
-setup-ohh-my-zsh="$script_dir/setup-oh-my-zsh.sh"
-setup-nodejs="$script_dir/setup-nodejs.sh"
 
-echo "Script directory: $script_dir"
+# function that checks if response is valid (Y/n)
+function check_response {
+    if [ "$1" = "Y" ] || [ "$1" = "y" ] || [ "$1" = "N" ] || [ "$1" = "n" ] || [ "$1" = "" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# ask user if they want to install docker
-echo "Do you want to install docker? (Y/n)"
-read install_docker
+# function that checks if response is valid version number
+function check_version {
+    if [[ "$1" =~ ^[0-9]+(\.[0-9]+)*$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# ========= GET USER INPUT =========
 
 # ask user if they want to install oh-my-zsh
 echo "Do you want to install oh-my-zsh? (Y/n)"
 read install_oh_my_zsh
 
+# throw error if response is invalid
+if ! check_response $install_oh_my_zsh; then
+    echo "Invalid response: $install_oh_my_zsh"
+    exit 1
+fi
+
+# ask user if they want to install docker
+echo "Do you want to install docker? (Y/n)"
+read install_docker
+
+# throw error if response is invalid
+if ! check_response $install_docker; then
+    echo "Invalid response: $install_docker"
+    exit 1
+fi
+
 # ask user if they want to install nodejs
 echo "Do you want to install nodejs? (Y/n)"
 read install_nodejs
 
-# update apt
-echo "Updating apt..."
-sudo apt update
+# throw error if response is invalid
+if ! check_response $install_nodejs; then
+    echo "Invalid response: $install_nodejs"
+    exit 1
+fi
 
 # if user wants to install nodejs, ask for version
 if [ "$install_nodejs" = "Y" ] || [ "$install_nodejs" = "y" ] || [ "$install_nodejs" = "" ]; then
     echo "Which version of nodejs do you want to install? (leave blank for latest)"
     read node_version
+
+    # throw error if version is invalid and not blank
+    if ! check_version $node_version; then
+        if ! [ "$node_version" = "" ]; then
+            echo "Invalid version number: $node_version"
+            exit 1
+        fi
+    fi
 fi
+
+# ask user if they want to install python
+echo "Do you want to install python? (Y/n)"
+read install_python
+
+# throw error if response is invalid
+if ! check_response $install_python; then
+    echo "Invalid response: $install_python"
+    exit 1
+fi
+
+# ========= RUN INSTALL SCRIPTS =========
+
+# update apt
+echo "Updating apt..."
+sudo apt update
 
 # run basic install script
 echo "Running basic install script..."
-chmod +x $setup-basics
-./$setup-basics
-
-# run docker install script
-if [ "$install_docker" = "Y" ] || [ "$install_docker" = "y" ] || [ "$install_docker" = "" ]; then
-    echo "Running docker install script..."
-    chmod +x $setup-docker
-    ./$setup-docker
-fi
+chmod +x $script_dir/setup-basics.sh
+./$script_dir/setup-basics.sh
 
 # run oh-my-zsh install script
 if [ "$install_oh_my_zsh" = "Y" ] || [ "$install_oh_my_zsh" = "y" ] || [ "$install_oh_my_zsh" = "" ]; then
     echo "Running oh-my-zsh install script..."
-    chmod +x $setup-ohh-my-zsh
-    ./$setup-ohh-my-zsh
+    chmod +x $script_dir/setup-oh-my-zsh.sh
+    ./$script_dir/setup-oh-my-zsh.sh
+fi
+
+# run docker install script
+if [ "$install_docker" = "Y" ] || [ "$install_docker" = "y" ] || [ "$install_docker" = "" ]; then
+    echo "Running docker install script..."
+    chmod +x $script_dir/setup-docker-engine.sh
+    ./$script_dir/setup-docker-engine.sh
 fi
 
 # run nodejs install script (note: setup-nodejs script takes nodejs version as argument)
 if [ "$install_nodejs" = "Y" ] || [ "$install_nodejs" = "y" ] || [ "$install_nodejs" = "" ]; then
     echo "Running nodejs install script..."
-    chmod +x $setup-nodejs
-    ./$setup-nodejs $node_version
+    chmod +x $script_dir/setup-nodejs.sh
+    ./$script_dir/setup-nodejs.sh $node_version
+fi
+
+# run python install script
+if [ "$install_python" = "Y" ] || [ "$install_python" = "y" ] || [ "$install_python" = "" ]; then
+    echo "Running python install script..."
+    chmod +x $script_dir/setup-python.sh
+    ./$script_dir/setup-python.sh
 fi
