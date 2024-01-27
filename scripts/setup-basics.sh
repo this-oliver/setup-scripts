d2519768ac8a16608481de4e4bb17ff43802a3ae
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-01-27 01:17:27 +0100

    adds alias for batcat

--- scripts/setup-basics.sh
+++ scripts/setup-basics.sh
@@ -3,3 +3,21 @@ sudo apt update
 
 # install basic tools
 sudo apt install curl git bat nmap -y
+
+# check if bat is renamed to batcat
+batcat_path=$(which batcat)
+
+# if batcat is found, set alias for batcat
+if [ "$batcat_path" != "" ]; then
+    # find if zsh is installed
+    zsh_path=$(which zsh)
+
+    # set alias for bat (note: some debian based distros use batcat instead of bat)
+    if [ "$zsh_path" != "" ]; then
+        echo "alias bat=batcat" >> ~/.zshrc
+    fi
+
+    if [ "$zsh_path" = "" ]; then
+        echo "alias bat=batcat" >> ~/.bashrc
+    fi
+fi
