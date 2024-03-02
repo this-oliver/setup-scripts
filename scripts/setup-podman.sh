23a5cfbf210fb85985797563b9ce66c3c869bf77
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-03-02 12:22:45 +0100

    feature: adds podman setup script

---
+++ scripts/setup-podman.sh
@@ -0,0 +1,14 @@
+# Podman is a containerization platform, like Docker, that packages
+# your application and all its dependencies together into software
+# packages called containers which can be run on any machine that also
+# has Podman installed. Unlike Docker, Podman does not require a
+# daemon, with root privileges, to manage containers which mitigates
+# security concerns. Apart from that, Podman is compatible with Docker
+# and can run Docker containers and images.
+
+echo "installing podman..."
+
+sudo apt update
+sudo apt install podman -y
+
+echo "podman installed successfully"
